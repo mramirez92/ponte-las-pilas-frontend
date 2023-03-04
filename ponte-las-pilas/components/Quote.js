b40ceb915fee71557d1210ddb00c2fab1f7407e4
mramirez92 <ramirez.92.monica@gmail.com>
2023-03-03 19:35:39 -0600

    frontend basic

---
+++ ponte-las-pilas/components/Quote.js
@@ -0,0 +1,11 @@
+export default function Quote() {
+    return (
+        <div className="p-12 flex items-center justify-center grow-0 max-w-lg ">
+            <h1 className="sm:text-lg md:text-xl lg:text-3xl xl:text-5xl text-center align-middle whitespace-normal text-slate-200 font-bold">
+                <span className="font-bold font-quotes font-Economica sm:text-lg md:text-xl lg:text-3xl xl:text-5xl text-slate-200">&ldquo; </span>Ponte las pilas!
+                <span className="font-bold font-quotes font-Economica sm:text-lg md:text-xl lg:text-3xl xl:text-5xl text-slate-200">&quot;</span>
+
+            </h1>
+        </div>
+    )
+}
--- ponte-las-pilas/pages/_document.js
+++ ponte-las-pilas/pages/_document.js
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="preconnect"  href="https://fonts.googleapis.com/css2?family=Economica&family=Urbanist&display=swap" />
+          <link  href="https://fonts.googleapis.com/css2?family=Economica&family=Urbanist&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
