99c00ab8156d3bd7e4034e13e13311d7cf2b5d2a
mramirez92 <ramirez.92.monica@gmail.com>
2023-03-24 12:08:36 -0500

    change to tailwind, gitignore

--- ponte-las-pilas/tailwind.config.js
+++ ponte-las-pilas/tailwind.config.js
@@ -14,7 +14,15 @@ module.exports = {
         sans:["Urbanist","sans-serif"],
         quotes: ["Economica", "sans-serif"],
         worksans:["Work+Sans", "sans-serif"],
-      }
+      },
+      colors: {
+        "bg-color-1": "bg-amber-500",
+        "bg-color-2": "bg-indigo-600",
+        "bg-color-3": "bg-red-400",
+        "bg-color-4": "bg-sky-500",
+        "bg-color-5": "bg-teal-500",
+        "bg-color-6": "bg-rose-400",
+      },
     },
   },
   plugins: [],
