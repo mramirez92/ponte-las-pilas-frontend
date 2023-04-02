ecb19715dd3ab74663abcf4c3696b1fba11962e3
mramirez92 <ramirez.92.monica@gmail.com>
2023-04-01 22:18:19 -0500

    edit to background color and naming conventions

--- ponte-las-pilas/pages/index.js
+++ ponte-las-pilas/pages/index.js
@@ -3,40 +3,58 @@ import { useEffect } from 'react';
 import Quote from '@/components/Quote'
 import GithubButton from '@/components/GithubButton';
 import MadeBy from '@/components/Madeby';
-// import styles from '@/styles/Home.module.css'
-
 import Head from 'next/head';
-export default function Home() {
-  useEffect(() => {
-    // Add the bg-rose-300 class to the html and body elements
-    document.documentElement.classList.add('bg-red-400');
-    document.body.classList.add('bg-red-400');
 
-    // Remove the class on unmount
+const colorsList = [
+  'bg-red-400',
+  'bg-sky-500',
+  'bg-emerald-500',
+  'bg-teal-500',
+  'bg-amber-500',
+  'bg-rose-400',
+  'bg-indigo-500',
+
+];
+
+const getRandomColor = () => {
+  const randomIndex = Math.floor(Math.random() * colorsList.length);
+  return colorsList[randomIndex];
+};
+
+const BackgroundColor = () => {
+  useEffect(() => {
+    const randomColor = getRandomColor();
+    document.documentElement.classList.add(randomColor);
+    document.body.classList.add(randomColor);
     return () => {
-      document.documentElement.classList.remove('bg-red-400');
-      document.body.classList.remove('bg-red-400');
+      document.documentElement.classList.remove(randomColor);
+      document.body.classList.remove(randomColor);
     };
   }, []);
 
+  return null;
+};
+
+export default function Home() {
   return (
     <>
-    <Head>
-    <title>New Tab</title>
-    <meta property="og:title" content="New Tab" key="title" />
-    <link rel="icon" href="/garland.png" />
-  </Head>
+    <BackgroundColor />
+      <Head>
+        <title>New Tab</title>
+        <meta property="og:title" content="New Tab" key="title" />
+        <link rel="icon" href="/garland.png" />
+      </Head>
+      
 
-    <div className="flex flex-col justify-center items-center h-screen pl-8 pr-8 m-auto">
-      <Quote className="w-1/2 h-auto self-center" />
-      <div className='self-end absolute bottom-6 left-5'>
-        <MadeBy/>
+      <div className="flex flex-col justify-center items-center h-screen pl-8 pr-8 m-auto">
+          <Quote />
+        <div className='self-end absolute bottom-6 left-5'>
+          <MadeBy/>
+        </div>
+        <div className= "absolute bottom-4 right-3">
+          <GithubButton  className= ""/>
+        </div>
       </div>
-      <div className= "absolute bottom-4 right-3">
-      <GithubButton  className= ""/>
-      </div>
-    </div>
-
-</>
+    </>
   );
 }
