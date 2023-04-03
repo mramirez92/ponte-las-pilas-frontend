2b4af4721c4ee41bd6dc84733d68107be952362a
mramirez92 <ramirez.92.monica@gmail.com>
2023-04-03 13:15:36 -0500

    change to button, console logs for debugging

--- ponte-las-pilas/components/GithubButton.js
+++ ponte-las-pilas/components/GithubButton.js
@@ -3,7 +3,7 @@ export default function GithubButton() {
     return (
         <Link href="https://ponte-las-pilas-api-mramirez92.vercel.app/">
 
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" className="w-6 h-6 hover:scale-125 hover:stroke-slate-900 hover:fill-slate-100 stroke-slate-100/60">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" className="w-6 h-6 hover:scale-125 hover:animate-pulse stroke-slate-100/70">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"/>
             </svg>
 
